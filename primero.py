26a228135385a725ca5abe74123d399a413a029b
mafer564 <madelin_fernanda@hotmail.com>
2016-02-22 21:46:15 -0600

    Super python

---
+++ primero.py
@@ -0,0 +1,33 @@
+
+	#PRIMERO
+import math
+informacion = input("¿Cuántas gigabytes de informacion quieres respaldar?")
+a = int(1024)
+mb = int(informacion) * a
+
+
+b = int(700)
+res = mb / b
+final=print("el numero de cd's que necesitas son: ",math.ceil(res))
+		#SEGUNDO
+numero=input("Ingresa el numero para saber si es par\n")
+num=int(numero)
+if num%2 == 0:
+	print("si es par")
+else: 
+	print("ño es par :(")
+		#TERCERO
+n1=input("ingresa el primero numero\n")
+n2=input("ingresa el segundo numero\n")
+op=input("ingresa el operador aritmetico\n")
+res=int(n1)
+
+		#sexto
+mensaje=input("ingresa el mensaje\n")
+n=input("ingresa el valor de n:\n")
+nuevo=[" "]
+abecedario=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+for elem in abecedario:
+	elem=+[n]
+print(mensaje)
+print(nuevo)	
